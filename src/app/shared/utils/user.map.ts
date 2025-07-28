49658a86f09f331ed92a1fce11af0c4f1c76e20f
lesquel <lesquel1319@gmail.com>
2025-07-28 10:16:09 -0500

    fix: Update import paths to use '@' prefix for consistency

--- src/app/shared/utils/user.map.ts
+++ src/app/shared/utils/user.map.ts
@@ -1,4 +1,4 @@
-import { User } from "app/core/entities/user.entity";
+import { User } from "@app/core/entities/user.entity";
 import { UserDTO } from "../models/user.dto";
 
 export const toUser = (dto: UserDTO) => new User(dto);
