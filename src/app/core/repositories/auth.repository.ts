96052ec4a42dc5b7ed080cfd17d139fc881386ed
lesquel <lesquel1319@gmail.com>
2025-07-28 09:49:59 -0500

    fix: Simplify register method signature in AuthRepository for consistency

--- src/app/core/repositories/auth.repository.ts
+++ src/app/core/repositories/auth.repository.ts
@@ -3,10 +3,7 @@ import { User } from "../entities/user.entity";
 
 export abstract class AuthRepository {
   abstract login(email: string, password: string): Observable<User>;
-  abstract register(
-    email: string,
-    password: string
-  ): Observable<User>;
+  abstract register(email: string, password: string): Observable<User>;
   abstract current(): Observable<User | null>;
   abstract logout(): Observable<void>;
 }
