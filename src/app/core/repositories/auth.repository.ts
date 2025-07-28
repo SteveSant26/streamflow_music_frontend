e568614aa2011997a85e777777de2c274afcb5be
lesquel <lesquel1319@gmail.com>
2025-07-28 09:49:42 -0500

    feat: Add RegisterUseCase and DTOs for user registration functionality

--- src/app/core/repositories/auth.repository.ts
+++ src/app/core/repositories/auth.repository.ts
@@ -4,9 +4,8 @@ import { User } from "../entities/user.entity";
 export abstract class AuthRepository {
   abstract login(email: string, password: string): Observable<User>;
   abstract register(
-    name: string,
     email: string,
-    password: string,
+    password: string
   ): Observable<User>;
   abstract current(): Observable<User | null>;
   abstract logout(): Observable<void>;
