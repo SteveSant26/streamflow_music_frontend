b35acab1d9987ed72ba26bc55938bb3805f84389
lesquel <lesquel1319@gmail.com>
2025-07-28 10:15:13 -0500

    feat: Implement TokenStorageService with methods for token management

--- src/app/data/services/token.storage.service.ts
+++ src/app/data/services/token.storage.service.ts
@@ -1,6 +1,17 @@
 import { Injectable } from "@angular/core";
+import { environment } from "@sh";
 
 @Injectable({
   providedIn: "root",
 })
-export class TokenStorageService {}
+export class TokenStorageService {
+  setToken(token: string) {
+    localStorage.setItem(environment.STORAGE_TOKEN_KEY, token);
+  }
+  getToken(): string | null {
+    return localStorage.getItem(environment.STORAGE_TOKEN_KEY);
+  }
+  clearToken() {
+    localStorage.removeItem(environment.STORAGE_TOKEN_KEY);
+  }
+}
