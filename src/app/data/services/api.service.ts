b13b05eee69707bb0a2b34f7c1fce65e4e374ba4
lesquel <lesquel1319@gmail.com>
2025-07-28 10:07:38 -0500

    feat: Add httpResource method for enhanced API request handling

--- src/app/data/services/api.service.ts
+++ src/app/data/services/api.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, httpResource } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
 import { environment } from "../../shared/constants/environments/environment";
 
@@ -9,9 +9,15 @@ export class ApiService {
   get<T>(url: string) {
     return this.http.get<T>(environment.API_URL + url);
   }
+
+  getWithSignal<T>(url: string) {
+    return httpResource(() => environment.API_URL + url);
+  }
+
   post<T>(url: string, body: unknown) {
     return this.http.post<T>(environment.API_URL + url, body);
   }
+
   put<T>(url: string, body: unknown) {
     return this.http.put<T>(environment.API_URL + url, body);
   }
