c20137908bdd4a89163f6911cedd755613849e89
lesquel <lesquel1319@gmail.com>
2025-07-28 10:44:31 -0500

    refactor: Remove obsolete SupabaseClient test case

--- src/app/infrastructure/services/test/supabase.client.ts.spec.ts
+++
@@ -1,7 +0,0 @@
-import { SupabaseClient } from "./../supabase.client.ts";
-
-describe("SupabaseClient", () => {
-  it("should create an instance", () => {
-    expect(new SupabaseClient()).toBeTruthy();
-  });
-});
