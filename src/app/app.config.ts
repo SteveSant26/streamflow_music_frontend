93563bc9aac693e3240bc3ecea2f630e40e3d7b8
lesquel <lesquel1319@gmail.com>
2025-08-01 10:20:24 -0500

    refactor: remove unused auth interceptor and update HTTP interceptor configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,32 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import {
+  HTTP_INTERCEPTORS,
   provideHttpClient,
-  withInterceptors,
 } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { authInterceptor } from './shared/utils/auth-interceptor';
 import { authProviders } from './infrastructure/providers/auth.providers';
 import { playerProviders } from './infrastructure/providers/player.providers';
+import { AuthTokenInterceptor } from './infrastructure/interceptors/auth-token.interceptor';
+import { HttpErrorInterceptor } from './infrastructure/interceptors/http-error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(withInterceptors([authInterceptor])),
+    provideHttpClient(),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthTokenInterceptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpErrorInterceptor,
+      multi: true,
+    }
     ...authProviders,
     ...playerProviders,
   ],
